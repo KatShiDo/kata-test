5eb13bb69489a7e7e6ef8eae9dba9f949c017204
Nikita <poberezhnyuk.n.i@edu.mirea.ru>
2024-06-17 16:49:08 +0300

    Complete calculator

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,87 @@
+package org.example;
+
+import java.util.Arrays;
+
+class Calculator {
+
+    public static String[] romanNumbers = new String[] {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+
+    public static int calcArabic(int first, int second, String operation) {
+        return switch (operation) {
+            case "+" -> first + second;
+            case "-" -> first - second;
+            case "*" -> first * second;
+            case "/" -> first / second;
+            default -> throw new IllegalArgumentException("Unknown operation: " + operation);
+        };
+    }
+
+    public static String calcRoman(String first, String second, String operation) {
+        int firstInt = Arrays.asList(romanNumbers).indexOf(first) + 1;
+        int secondInt = Arrays.asList(romanNumbers).indexOf(second) + 1;
+
+        int result = calcArabic(firstInt, secondInt, operation);
+        if (result < 1) {
+            throw new ArithmeticException("Roman calculation failed: result is not positive");
+        }
+        return intToRoman(result);
+    }
+
+    public static String intToRoman(int value) {
+        if (value < 1 || value > 3999)
+            return "Invalid Roman Number Value";
+        StringBuilder s = new StringBuilder();
+        while (value >= 1000) {
+            s.append("M");
+            value -= 1000;        }
+        while (value >= 900) {
+            s.append("CM");
+            value -= 900;
+        }
+        while (value >= 500) {
+            s.append("D");
+            value -= 500;
+        }
+        while (value >= 400) {
+            s.append("CD");
+            value -= 400;
+        }
+        while (value >= 100) {
+            s.append("C");
+            value -= 100;
+        }
+        while (value >= 90) {
+            s.append("XC");
+            value -= 90;
+        }
+        while (value >= 50) {
+            s.append("L");
+            value -= 50;
+        }
+        while (value >= 40) {
+            s.append("XL");
+            value -= 40;
+        }
+        while (value >= 10) {
+            s.append("X");
+            value -= 10;
+        }
+        while (value >= 9) {
+            s.append("IX");
+            value -= 9;
+        }
+        while (value >= 5) {
+            s.append("V");
+            value -= 5;
+        }
+        while (value >= 4) {
+            s.append("IV");
+            value -= 4;
+        }
+        while (value >= 1) {
+            s.append("I");
+            value -= 1;
+        }
+        return s.toString();
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var in = new Scanner(System.in);
+        var input = in.nextLine();
+        System.out.println(calc(input));
+    }
+
+    public static String[] romanNumbers = new String[] {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+    public static String[] operations = new String[] {"+", "-", "*", "/"};
+
+    public static String calc(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("Empty input");
+        }
+        try {
+            var params = input.split(" ");
+            if (params.length != 3) {
+                throw new IllegalArgumentException("Input must contain three parameters");
+            }
+
+            var first = params[0];
+            var second = params[2];
+            var operation = params[1];
+
+            if (!Arrays.asList(operations).contains(operation)) {
+                throw new IllegalArgumentException("Invalid operation: " + operation);
+            }
+
+            if (Arrays.asList(romanNumbers).contains(first)) {
+                if (Arrays.asList(romanNumbers).contains(second)) {
+                    return Calculator.calcRoman(first, second, operation);
+                }
+                else {
+                    throw new IllegalArgumentException("Values must be both arabic or roman");
+                }
+            }
+
+            var firstArabic = Integer.parseInt(first);
+            var secondArabic = Integer.parseInt(second);
+            if (firstArabic < 1 || firstArabic > 10 || secondArabic < 1 || secondArabic > 10) {
+                throw new IllegalArgumentException("Values must be between 1 and 10");
+            }
+            return String.valueOf(Calculator.calcArabic(firstArabic, secondArabic, operation));
+        }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Values must be integers");
+        }
+        catch (ArithmeticException | IllegalArgumentException e) {
+            throw e;
+        }
+        catch (Exception e) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+    }
+}
